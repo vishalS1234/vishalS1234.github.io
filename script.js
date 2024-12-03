4981792a873cd6f36796886e10e47877715107bc
Vishal <vishal.saini@infinitysoftsystems.com>
2024-12-03 16:04:29 +0530

    added custom upload button on popup

--- script.js
+++ script.js
@@ -48,3 +48,8 @@ $(document).ready(function() {
     });
 });
 
+// upload file 
+function updateFileName(input) {
+    const fileName = input.files.length > 0 ? input.files[0].name : "No file chosen";
+    document.getElementById("fileName").textContent = fileName;
+}
